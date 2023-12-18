cac27508517505cd628b1ccdd3b184767c3a788c
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 21:05:02 -0500

    fix(zsh): add backslash in order to use newlines in shell scripts

--- zsh/.config/zsh/extras/reflector.zsh
+++ zsh/.config/zsh/extras/reflector.zsh
@@ -2,6 +2,6 @@
 command -v reflector &> /dev/null || return 0
 
 function update_mirrors() {
-	cp -f "/etc/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist.bak"
+	cp -f "/etc/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist.bak" \
 		&& reflector --fastest 30 --latest 30 --number 15 --verbose --save "/etc/pacman.d/mirrorlist"
 }
