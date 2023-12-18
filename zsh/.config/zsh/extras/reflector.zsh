38f248eb7e2db979fafd5bc9ab8368ca1c56a22c
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 21:03:17 -0500

    fix(zsh): run reflector only if mirrorlist was backed up successfully

--- zsh/.config/zsh/extras/reflector.zsh
+++ zsh/.config/zsh/extras/reflector.zsh
@@ -3,5 +3,5 @@ command -v reflector &> /dev/null || return 0
 
 function update_mirrors() {
 	cp -f "/etc/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist.bak"
-	reflector --fastest 30 --latest 30 --number 15 --verbose --save "/etc/pacman.d/mirrorlist"
+		&& reflector --fastest 30 --latest 30 --number 15 --verbose --save "/etc/pacman.d/mirrorlist"
 }
