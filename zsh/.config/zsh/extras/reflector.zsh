dc85bf85e07275143d1976d6ca96f38a4a30b91c
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 21:06:39 -0500

    fix(zsh): add sudo calls bc sudo can't invoke functions

--- zsh/.config/zsh/extras/reflector.zsh
+++ zsh/.config/zsh/extras/reflector.zsh
@@ -2,6 +2,6 @@
 command -v reflector &> /dev/null || return 0
 
 function update_mirrors() {
-	cp -f "/etc/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist.bak" \
-		&& reflector --fastest 30 --latest 30 --number 15 --verbose --save "/etc/pacman.d/mirrorlist"
+	sudo cp -f "/etc/pacman.d/mirrorlist" "/etc/pacman.d/mirrorlist.bak" \
+		&& sudo reflector --fastest 30 --latest 30 --number 15 --verbose --save "/etc/pacman.d/mirrorlist"
 }
