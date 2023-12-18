a8a4ca2c47d2a104a94ac7bed5d99ceeaa964665
Nobody4931 <ardenxie1@gmail.com>
2023-12-17 23:53:26 -0500

    fix(zsh): add alternate zsh plugin paths

--- zsh/.config/zsh/completion_suggestions.zsh
+++ zsh/.config/zsh/completion_suggestions.zsh
@@ -1,16 +1,20 @@
 # Source autosuggestions plugin if it exists
 if [[ -f "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
 	source "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-
-	ZSH_AUTOSUGGEST_USE_ASYNC=1
-	ZSH_AUTOSUGGEST_STRATEGY=("history" "completion")
-	ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
-
-	#ZSH_AUTOSUGGEST_CLEAR_WIDGETS=()
-	ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
-	ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(forward-char forward-word)
-	ZSH_AUTOSUGGEST_EXECUTE_WIDGETS=()
-	ZSH_AUTOSUGGEST_IGNORE_WIDGETS=()
-
-	bindkey '^s' autosuggest-accept
+elif [[ -f "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" ]]; then
+	source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+else
+	return 0
 fi
+
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+ZSH_AUTOSUGGEST_STRATEGY=("history" "completion")
+ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
+
+#ZSH_AUTOSUGGEST_CLEAR_WIDGETS=()
+ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=()
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(forward-char forward-word)
+ZSH_AUTOSUGGEST_EXECUTE_WIDGETS=()
+ZSH_AUTOSUGGEST_IGNORE_WIDGETS=()
+
+bindkey '^s' autosuggest-accept
--- zsh/.config/zsh/prompt_syntax.zsh
+++ zsh/.config/zsh/prompt_syntax.zsh
@@ -1,4 +1,6 @@
 # Source syntax highlighting plugin if it exists
 if [[ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
 	source "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+elif [[ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
+	source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 fi
