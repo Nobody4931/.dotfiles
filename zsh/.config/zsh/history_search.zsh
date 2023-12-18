d599d873fa479d702a243393eaf9318be048b323
Nobody4931 <ardenxie1@gmail.com>
2023-12-18 16:28:33 -0500

    fix(zsh): add alternate fzf plugin paths

--- zsh/.config/zsh/history_search.zsh
+++ zsh/.config/zsh/history_search.zsh
@@ -1,9 +1,13 @@
 # Source fzf's zsh keybinds if it exists
 if [[ -f "/usr/share/fzf/key-bindings.zsh" ]]; then
 	source "/usr/share/fzf/key-bindings.zsh"
-
-	# Remove unwanted keybinds (why is there no config option for this??)
-	for key in $(bindkey -L | grep fzf | grep -v history | cut -d ' ' -f 2 | tr -d '"'); do
-		bindkey -r $key
-	done
+elif [[ -f "/usr/share/doc/fzf/examples/key-bindings.zsh" ]]; then
+	source "/usr/share/doc/fzf/examples/key-bindings.zsh"
+else
+	return 0
 fi
+
+# Remove unwanted keybinds (why is there no config option for this??)
+for key in $(bindkey -L | grep fzf | grep -v history | cut -d ' ' -f 2 | tr -d '"'); do
+	bindkey -r $key
+done
