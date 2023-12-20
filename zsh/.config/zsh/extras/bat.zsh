8690f3ff8b1b7376ac0fc8113821b56165e109b3
Nobody4931 <ardenxie1@gmail.com>
2023-12-19 19:18:09 -0500

    fix: locate `batcat` if `bat` doesn't exist
    `bat` is renamed to `batcat` on Ubuntu/Debian systems because of name
    conflicts with another package

--- zsh/.config/zsh/extras/bat.zsh
+++ zsh/.config/zsh/extras/bat.zsh
@@ -1,5 +1,10 @@
-# Stop execution if bat is not installed
-command -v bat &> /dev/null || return 0
+# Stop execution if bat/batcat is not installed
+# NOTE: bat is renamed to batcat on Ubuntu/Debian because of name conflicts with another package
+command -v bat &> /dev/null \
+	|| command -v batcat &> /dev/null \
+	|| return 0
+
+batcmd=${commands[bat]:-${commands[batcat]}}
 
 # Add aliases
-alias cat="bat"
+alias cat="$batcmd"
