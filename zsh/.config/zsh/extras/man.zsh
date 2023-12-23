c849276e47aa310150bddbe48389aff259ae9d6e
Nobody4931 <ardenxie1@gmail.com>
2023-12-23 13:46:39 -0500

    fix(zsh): make colored man pages more global
    By setting `MANPAGER`, now man will always use custom colors no matter
    where it is being called from (ie. originally didn't work when using
    `git ... --help`)

--- zsh/.config/zsh/extras/man.zsh
+++ zsh/.config/zsh/extras/man.zsh
@@ -1,23 +1,15 @@
 # Stop execution if man is not installed
 command -v man &> /dev/null || return 0
 
-function colored_pager() {
-	local -a environment
+# Build MANPAGER environment
+local -a environment
+environment+=("LESS_TERMCAP_so=${fg_bold[yellow]}${bg[blue]}")
+environment+=("LESS_TERMCAP_mb=${fg_bold[magenta]}")
+environment+=("LESS_TERMCAP_md=${fg_bold[magenta]}")
+environment+=("LESS_TERMCAP_us=${fg_bold[blue]}")
+environment+=("LESS_TERMCAP_me=${reset_color}")
+environment+=("LESS_TERMCAP_se=${reset_color}")
+environment+=("LESS_TERMCAP_ue=${reset_color}")
+environment+=("GROFF_NO_SGR=1")
 
-	environment+=("LESS_TERMCAP_so=${fg_bold[yellow]}${bg[blue]}")
-	environment+=("LESS_TERMCAP_mb=${fg_bold[magenta]}")
-	environment+=("LESS_TERMCAP_md=${fg_bold[magenta]}")
-	environment+=("LESS_TERMCAP_us=${fg_bold[blue]}")
-	environment+=("LESS_TERMCAP_me=${reset_color}")
-	environment+=("LESS_TERMCAP_se=${reset_color}")
-	environment+=("LESS_TERMCAP_ue=${reset_color}")
-	environment+=("GROFF_NO_SGR=1")
-
-	environment+=("PAGER=${commands[less]:-$PAGER}")
-
-	command env $environment "$@"
-}
-
-function man() {
-	colored_pager $0 $@
-}
+export MANPAGER="${${commands[less]:+env $environment ${commands[less]}}:-$MANPAGER}"
