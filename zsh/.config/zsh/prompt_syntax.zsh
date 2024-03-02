2f05e027698147d2d79374ac6d702888c3c9859e
Nobody4931 <ardenxie1@gmail.com>
2024-03-02 17:51:21 -0500

    style(zsh): add consistency to file sourcing

--- zsh/.config/zsh/prompt_syntax.zsh
+++ zsh/.config/zsh/prompt_syntax.zsh
@@ -3,4 +3,6 @@ if [[ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting
 	source "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 elif [[ -f "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]]; then
 	source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+else
+	return 0
 fi
