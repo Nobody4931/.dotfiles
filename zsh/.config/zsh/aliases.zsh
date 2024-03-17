e1f85156bca901519aed96018f99396a081021e4
Nobody4931 <ardenxie1@gmail.com>
2024-03-17 14:15:01 -0400

    feat(zsh): add working dir backtrack aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,6 +7,10 @@ alias l="ls -lF"
 alias la="ls -lFA"
 alias lsa="ls -a"
 
+# Quick cwd backtracking
+alias ..="cd .."
+alias ...="cd ../.."
+
 # Confirm before overwriting
 alias cp="cp -i"
 alias mv="mv -i"
